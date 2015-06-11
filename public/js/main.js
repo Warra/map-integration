395dba804219b3dfc3682ab37f34ae0fa1b6ad9b
Warra <warrenmanley@gmail.com>
2015-06-11 11:47:02 +0200

    adds searchbox to query map

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,23 @@
 $(function(){
     function initialize() {
         var mapOptions = {
-          center: { lat: -34.397, lng: 150.644},
+          center: { lat: -33.9341655, lng: 18.4571285},
           zoom: 8
         };
+
         var map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
+
+        var marker = new google.maps.Marker({
+            position: {
+                lat: -33.9341655,
+                lng: 18.4571285
+            },
+            map: map,
+            draggable: true
+        });
+        var searchBox = new google.maps.places.SearchBox(document.getElementById('location'));
+
       }
       google.maps.event.addDomListener(window, 'load', initialize);
 });
--- resources/views/MapForm.blade.php
+++ resources/views/MapForm.blade.php
@@ -10,7 +10,7 @@
         <div class="col-md-6">
         {!! Form::open() !!}
             {!! Form::label('location', 'Location', ['class' => 'label']) !!}
-            {!! Form::text('location', '', ['class' => 'form-control']) !!} <br />
+            {!! Form::text('location', '', ['class' => 'form-control', 'id'=>'location']) !!} <br />
             {!! Form::submit('Use Location') !!}
         {!! Form::close() !!}
         </div>
@@ -21,6 +21,7 @@
     
 </body>
     {!! Html::script('http://code.jquery.com/jquery-1.11.3.min.js') !!}
-    {!! Html::script('https://maps.googleapis.com/maps/api/js?key=AIzaSyCnB9CM1raIla-HdXTM_8fwnLtN2rY65Ig') !!}
+    {!! Html::script('https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyCnB9CM1raIla-HdXTM_8fwnLtN2rY65Ig') !!}
+
     {!! Html::script('js/main.js') !!}
 </html>
