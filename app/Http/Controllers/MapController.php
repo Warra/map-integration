dcb8aa02d07705761df11212db5fcac75d32237e
Warra <warrenmanley@gmail.com>
2015-06-11 10:34:02 +0200

    adds map form

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
-abstract class MapController extends Controller
+class MapController extends Controller
 {
     use DispatchesJobs, ValidatesRequests;
+    
+    public function show()
+    {
+        return view('MapForm');
+    }
+
 }
 
-public function show()
-{
-    return view('MapForm');
-}
+
--- resources/views/MapForm.blade.php
+++ resources/views/MapForm.blade.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-    
     <title>Map Integration</title>
+    {!! Html::style('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css') !!}
+    {!! Html::style('css/main.css') !!}
 </head>
 <body>
-    <div>
-        
+    <div class="all-padding">
+        <div class="col-md-6">
+        {!! Form::open() !!}
+            {!! Form::label('location', 'Location', ['class' => 'label']) !!}
+            {!! Form::text('location', '', ['class' => 'form-control']) !!} <br />
+            {!! Form::submit('Use Location') !!}
+        {!! Form::close() !!}
+        </div>
+        <div id="map-canvas" class="col-md-6 map-section">
+            
+        </div>
     </div>
+    
 </body>
+    {!! Html::script('http://code.jquery.com/jquery-1.11.3.min.js') !!}
+    {!! Html::script('https://maps.googleapis.com/maps/api/js?key=AIzaSyCnB9CM1raIla-HdXTM_8fwnLtN2rY65Ig') !!}
+    {!! Html::script('js/main.js') !!}
 </html>
